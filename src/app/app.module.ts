ff4df1cbdd20eebc9eb71393863108bba7263fea
besika94 <besika94@gmail.com>
2024-06-22 11:43:34 +0100

    migrate to angular v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { NgwWowModule } from 'ngx-wow';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -26,36 +26,29 @@ import { AuthComponent } from './pages/auth/auth.component';
 import { TextInputComponent } from './components/forms/text-input/text-input.component';
 import { RegisterFormComponent } from './components/forms/register-form/register-form.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    CardComponent,
-    FooterComponent,
-    CartComponent,
-    ProductsComponent,
-    ProductComponent,
-    SliderComponent,
-    AuthComponent,
-    TextInputComponent,
-    RegisterFormComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    NgwWowModule,
-    NgbModule,
-    NgxPaginationModule,
-    SwiperModule,
-    BrowserAnimationsModule,
-    MatSnackBarModule,
-    NgSelectModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        CardComponent,
+        FooterComponent,
+        CartComponent,
+        ProductsComponent,
+        ProductComponent,
+        SliderComponent,
+        AuthComponent,
+        TextInputComponent,
+        RegisterFormComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgwWowModule,
+        NgbModule,
+        NgxPaginationModule,
+        SwiperModule,
+        BrowserAnimationsModule,
+        MatSnackBarModule,
+        NgSelectModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
